9272f531e3f5b46ebbf1aee7e3767d49c59aca97
kepleroso <amotta12345@gmail.com>
2026-04-23 09:39:49 +0200

    Update server.js

--- qgbags-backend/src/server.js
+++ qgbags-backend/src/server.js
@@ -5,7 +5,7 @@
 require('dotenv').config();
 const express  = require('express');
 const cors     = require('cors');
-const fetch    = (...args) => import('node-fetch').then(({default: f}) => f(...args));
+const fetch    = const fetch = globalThis.fetch;
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
